08d33c0dc42fc2c624667c457a3bc8d01b67558c
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-04-25 12:16:16 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@ git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
 
 echo "*************************** Go To SoraStatus Directory ***************************"
 cd ./sorastatus
-# git remote set-url --add --push origin https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
-# git remote set-url --add --push origin https://IzumiSenaSora:$GH_TOKEN@github.com/IzumiSenaSora/SoraStatus.git
-# git remote set-url --add --push origin https://UnOrdinary:$GITLAB_TOKEN@gitlab.com/SoraStatus/SoraStatus.git
+git remote set-url --add --push origin https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
+git remote set-url --add --push origin https://IzumiSenaSora:$GH_TOKEN@github.com/IzumiSenaSora/SoraStatus.git
+git remote set-url --add --push origin https://UnOrdinary:$GITLAB_TOKEN@gitlab.com/SoraStatus/SoraStatus.git
 
 echo "*************************** Show Git Remote Lists ***************************"
 git remote -v
