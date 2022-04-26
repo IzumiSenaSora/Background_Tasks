34e46b0a53ef4fd844fd61eb8419e45da8f3ea0a
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-04-26 13:58:27 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
+date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
+
 echo "*************************** Clone SoraStatus Repo ***************************"
 git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
 
 echo "*************************** Go To SoraStatus Directory ***************************"
 cd ./sorastatus
-git remote set-url --add --push origin https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
-git remote set-url --add --push origin https://IzumiSenaSora:$GH_TOKEN@github.com/IzumiSenaSora/SoraStatus.git
-git remote set-url --add --push origin https://UnOrdinary:$GITLAB_TOKEN@gitlab.com/SoraStatus/SoraStatus.git
 
-echo "*************************** Show Git Remote Lists ***************************"
-git remote -v
+echo "*************************** Clone SoraStatus Logs Repo ***************************"
+git clone https://IzumiSenaSora:$GH_TOKEN@github.com/IzumiSenaSora/SoraStatus_Logs.git
+
+mv SoraStatus_Logs logs
 
 echo "*************************** Run SoraStatus Health Checks ***************************"
 bash health-check.sh
+
+cd ./logs
+
+# Git Commit
+echo "Git Commit"
+git config --global user.name 'Izumi Sena Sora'
+git config --global user.email '$EMAIL'
+git add ./
+git commit -m "[Automated] Update SoraStatus Logs $date"
+git push -u origin main
