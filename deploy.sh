853bc8b4caad65aa629e050369bc8ccc372b5f23
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-04-26 22:55:02 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
 date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 
-echo "*************************** Clone SoraStatus Repo ***************************"
+echo " *** Clone SoraStatus Repo *** "
 git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/lotns/sorastatus
 
-echo "*************************** Go To SoraStatus Directory ***************************"
+echo " *** Go To SoraStatus Directory *** "
 cd ./sorastatus
 
-echo "*************************** Clone SoraStatus Logs Repo ***************************"
+echo " *** Clone SoraStatus Logs Repo *** "
 git clone https://IzumiSenaSora:$GH_TOKEN@github.com/IzumiSenaSora/SoraStatus_Logs.git
 
+echo " *** Rename SoraStatus_Logs Into logs *** "
 mv SoraStatus_Logs logs
 
-echo "*************************** Run SoraStatus Health Checks ***************************"
+echo " *** Run SoraStatus Health Checks *** "
 bash health-check.sh
 
 cd ./logs
@@ -22,4 +23,4 @@ git config --global user.name 'Izumi Sena Sora'
 git config --global user.email '$EMAIL'
 git add .
 git commit -m "[Automated] Update SoraStatus Logs $date"
-git push -u origin main
+git push
