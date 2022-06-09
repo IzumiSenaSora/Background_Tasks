de0429a9e8337b898c5e563126d4d309fbb66d5b
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-09 22:12:55 +0600

    Update sorastatus.sh

--- sorastatus/sorastatus.sh
+++ sorastatus/sorastatus.sh
@@ -26,6 +26,40 @@ bash health-check.sh
 
 cd logs
 
+cp LOTNS_report.log AeonQuake_report.log
+cp LOTNS_report.log Caelum_report.log
+cp LOTNS_report.log Easin_report.log
+cp LOTNS_report.log Handshake_report.log
+cp LOTNS_report.log Izumi_report.log
+cp LOTNS_report.log IzumiSena_report.log
+cp LOTNS_report.log IzumiSenaSora_report.log
+cp LOTNS_report.log LightOfTheNightSky_report.log
+cp LOTNS_report.log LightOfTheNightSkyFoundation_report.log
+# cp LOTNS_report.log LOTNS_report.log
+cp LOTNS_report.log LOTNSFoundation_report.log
+cp LOTNS_report.log Messege_report.log
+cp LOTNS_report.log NazmusSadat_report.log
+cp LOTNS_report.log NotRyan_report.log
+cp LOTNS_report.log NSOne_report.log
+cp LOTNS_report.log NSTwo_report.log
+cp LOTNS_report.log PixelFed_report.log
+cp LOTNS_report.log PrivateRelay_report.log
+cp LOTNS_report.log Sanjoy_report.log
+cp LOTNS_report.log Shishir_report.log
+# cp LOTNS_report.log SoraAPI_report.log
+# cp LOTNS_report.log SoraBlog_report.log
+cp LOTNS_report.log SoraCloud_report.log
+cp LOTNS_report.log SoraDNS_report.log
+# cp LOTNS_report.log SoraFonts_report.log
+cp LOTNS_report.log SoraFoundation_report.log
+# cp LOTNS_report.log SoraID_report.log
+cp LOTNS_report.log SoraMail_report.log
+cp LOTNS_report.log SoraOS_report.log
+# cp LOTNS_report.log SoraStatus_report.log
+cp LOTNS_report.log SoraStatusLogs_report.log
+# cp LOTNS_report.log UnOrdinary_report.log
+cp LOTNS_report.log Zinik_report.log
+
 echo " *** Show Git Remote Lists Of SoraStatus Logs *** "
 git remote -v
 
