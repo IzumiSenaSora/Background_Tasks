983a9793aee2e4afaba8c5cc77e829f357b498b6
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-16 00:14:03 +0600

    Update git.sh

--- git/git.sh
+++ git/git.sh
@@ -6,9 +6,23 @@ git config --global user.email "$EMAIL"
 echo " *** Go To Git Directory *** "
 cd ./git
 
+echo " *** CLONE *** "
 bash ./clone.sh
+
+echo " *** REMOTE *** "
 bash ./remote.sh
+
+# echo " *** FETCH *** "
 # bash ./fetch.sh
+
+# echo " *** MERGE *** "
 # bash ./merge.sh
+
+echo " *** PULL *** "
 bash ./pull.sh
+
+# echo " *** CHECKOUT *** "
+# bash ./checkout.sh
+
+echo " *** PUSH *** "
 bash ./push.sh
