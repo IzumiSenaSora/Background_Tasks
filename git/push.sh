1d9a47180222db89e2d267533c2927c1751d87ed
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-15 12:35:24 +0600

    Update push.sh

--- git/push.sh
+++ git/push.sh
@@ -4,3 +4,175 @@ SPACEEnd="*************************** Ending ***************************"
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
+
+echo "*************************** PUSH ***************************"
+echo "$SPACEStart"
+echo "LOTNS Server Setup"
+cd ./lotns_server_setup
+git add .
+git commit -m "Update LOTNS Server Setup $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "OpenSSL"
+cd ./openssl
+git add .
+git commit -m "Update OpenSSL $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "DNSControl"
+cd ./dnscontrol
+git add .
+git commit -m "Update DNSControl $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "About Soracloud"
+cd ./about_soracloud
+git add .
+git commit -m "Update About Soracloud $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraCloud Themes"
+cd ./soracloud_themes
+git add .
+git commit -m "Update SoraCloud Themes $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraAPI"
+cd ./soraapi
+git add .
+git commit -m "Update SoraAPI $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraBlog"
+cd ./sorablog
+git add .
+git commit -m "Update SoraBlog $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraFonts"
+cd ./sorafonts
+git add .
+git commit -m "Update SoraFonts $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS Web Server"
+cd ./lotns_web_server
+git add .
+git commit -m "Update LOTNS Web Server $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS"
+cd ./lotns
+git add .
+git commit -m "Update LOTNS $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "UnOrdinary"
+cd ./unordinary
+git add .
+git commit -m "Update UnOrdinary $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID"
+cd ./soraid
+git add .
+git commit -m "Update SoraID $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID UnProtected"
+cd ./soraid_unprotected
+git add .
+git commit -m "Update SoraID $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Public"
+cd ./sorastatus
+git add .
+git commit -m "Update SoraStatus Public $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Logs"
+cd ./sorastatus_logs
+git add .
+git commit -m "Update SoraStatus Logs $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Private"
+cd ./sorastatus_private
+git add .
+git commit -m "Update SoraStatus Private $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "ReWrites"
+cd ./rewrites
+git add .
+git commit -m "Update ReWrites $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "Let's Encrypt"
+cd ./letsencrypt
+git add .
+git commit -m "Update Let's Encrypt $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "AeonQuake"
+cd ./aeonquake
+git add .
+git commit -m "Update AeonQuake $date"
+git push origin master
+echo "$SPACEEnd"
+cd ..
