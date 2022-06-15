110cb12215d8d7d5fb79c68450f44c2559c0b8ef
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-15 16:09:39 +0600

    Update clone.sh

--- git/clone.sh
+++ git/clone.sh
@@ -1,6 +1,6 @@
 date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
-SPACEStart=" *** Starting ***"
-SPACEEnd=" *** Ending ***"
+SPACEStart=" *** Starting *** "
+SPACEEnd=" *** Ending *** "
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
