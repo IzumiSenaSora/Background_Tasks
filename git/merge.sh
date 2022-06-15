010f775ef5d9ab782eef1afbf85fe32605afe26d
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-15 12:38:49 +0600

    Update merge.sh

--- git/merge.sh
+++ git/merge.sh
@@ -4,3 +4,130 @@ SPACEEnd="*************************** Ending ***************************"
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
+
+echo "*************************** MERGE ***************************"
+echo "$SPACEStart"
+echo "LOTNS Server Setup"
+cd ./lotns_server_setup
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "OpenSSL"
+cd ./openssl
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "DNSControl"
+cd ./dnscontrol
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "About Soracloud"
+cd ./about_soracloud
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraCloud Themes"
+cd ./soracloud_themes
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraAPI"
+cd ./soraapi
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraBlog"
+cd ./sorablog
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraFonts"
+cd ./sorafonts
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS Web Server"
+cd ./lotns_web_server
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS"
+cd ./lotns
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "UnOrdinary"
+cd ./unordinary
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID"
+cd ./soraid
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID UnProtected"
+cd ./soraid_unprotected
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Public"
+cd ./sorastatus
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Logs"
+cd ./sorastatus_logs
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Private"
+cd ./sorastatus_private
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "ReWrites"
+cd ./rewrites
+git merge
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "AeonQuake"
+cd ./aeonquake
+git merge
+echo "$SPACEEnd"
+cd ..
