6e489ff90ea3857eff47e317c17cfecb0e9a25ad
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-15 12:25:53 +0600

    Create fetch.sh

---
+++ git/fetch.sh
@@ -0,0 +1,6 @@
+date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
+SPACEStart="*************************** Starting ***************************"
+SPACEEnd="*************************** Ending ***************************"
+
+git config --global user.name "Izumi Sena Sora"
+git config --global user.email "$EMAIL"
