01797ca43e4c8d76416859f0a96f37589fa385bc
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-06-15 12:36:47 +0600

    Update pull.sh

--- git/pull.sh
+++ git/pull.sh
@@ -4,3 +4,137 @@ SPACEEnd="*************************** Ending ***************************"
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
+
+echo "*************************** PULL ***************************"
+echo "$SPACEStart"
+echo "LOTNS Server Setup"
+cd ./lotns_server_setup
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "OpenSSL"
+cd ./openssl
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "DNSControl"
+cd ./dnscontrol
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "About Soracloud"
+cd ./about_soracloud
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraCloud Themes"
+cd ./soracloud_themes
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraAPI"
+cd ./soraapi
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraBlog"
+cd ./sorablog
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraFonts"
+cd ./sorafonts
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS Web Server"
+cd ./lotns_web_server
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "LOTNS"
+cd ./lotns
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "UnOrdinary"
+cd ./unordinary
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID"
+cd ./soraid
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraID UnProtected"
+cd ./soraid_unprotected
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Public"
+cd ./sorastatus
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Logs"
+cd ./sorastatus_logs
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "SoraStatus Private"
+cd ./sorastatus_private
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "ReWrites"
+cd ./rewrites
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "Let's Encrypt"
+cd ./letsencrypt
+git pull
+echo "$SPACEEnd"
+cd ..
+
+echo "$SPACEStart"
+echo "AeonQuake"
+cd ./aeonquake
+git pull
+echo "$SPACEEnd"
+cd ..
