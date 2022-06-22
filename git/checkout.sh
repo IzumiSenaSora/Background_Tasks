84caa033a4871eadd082bbdd0468a4257e184581
Izumi Sena Sora <hello@unordinary.eu.org>
2022-06-23 00:32:36 +0600

    done

--- git/checkout.sh
+++ git/checkout.sh
@@ -9,11 +9,11 @@ echo " *** CHECKOUT *** "
 echo "$SPACEStart"
 echo "LOTNS Server Setup"
 cd ./lotns_server_setup
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo LOTNS Server Setup $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -21,11 +21,11 @@ cd ..
 echo "$SPACEStart"
 echo "OpenSSL"
 cd ./openssl
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo OpenSSL $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -33,11 +33,11 @@ cd ..
 echo "$SPACEStart"
 echo "DNSControl"
 cd ./dnscontrol
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo DNSControl $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -45,11 +45,11 @@ cd ..
 echo "$SPACEStart"
 echo "About Soracloud"
 cd ./about_soracloud
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo About Soracloud $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -57,11 +57,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraCloud Themes"
 cd ./soracloud_themes
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraCloud Themes $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -69,11 +69,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraAPI"
 cd ./soraapi
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraAPI $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -81,11 +81,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraBlog"
 cd ./sorablog
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraBlog $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -93,11 +93,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraFonts"
 cd ./sorafonts
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraFonts $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -105,11 +105,11 @@ cd ..
 echo "$SPACEStart"
 echo "LOTNS Web Server"
 cd ./lotns_web_server
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo LOTNS Web Server $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -117,11 +117,11 @@ cd ..
 echo "$SPACEStart"
 echo "LOTNS"
 cd ./lotns
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo LOTNS $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -129,11 +129,11 @@ cd ..
 echo "$SPACEStart"
 echo "UnOrdinary"
 cd ./unordinary
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo UnOrdinary $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -141,11 +141,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraID"
 cd ./soraid
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraID $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -153,11 +153,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraID UnProtected"
 cd ./soraid_unprotected
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraID $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -165,11 +165,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraLicense"
 cd ./soralicense
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraLicense $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -177,11 +177,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraStatus Public"
 cd ./sorastatus
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraStatus Public $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -189,11 +189,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraStatus Logs"
 cd ./sorastatus_logs
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraStatus Logs $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -201,11 +201,11 @@ cd ..
 echo "$SPACEStart"
 echo "SoraStatus Private"
 cd ./sorastatus_private
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo SoraStatus Private $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -213,11 +213,11 @@ cd ..
 echo "$SPACEStart"
 echo "ReWrites"
 cd ./rewrites
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo ReWrites $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
@@ -225,11 +225,11 @@ cd ..
 # echo "$SPACEStart"
 # echo "Let's Encrypt"
 # cd ./letsencrypt
-# git checkout --orphan main
+# git checkout --orphan master
 # git add --all
 # git commit --all -m "Initialize Repo Let's Encrypt $date"
-# git branch -D master
-# # git branch -m master
+# git branch -D main
+# git branch -m main
 # git push -f origin main
 # echo "$SPACEEnd"
 # cd ..
@@ -237,11 +237,11 @@ cd ..
 echo "$SPACEStart"
 echo "AeonQuake"
 cd ./aeonquake
-git checkout --orphan main
+git checkout --orphan master
 git add --all
 git commit --all -m "Initialize Repo AeonQuake $date"
-git branch -D master
-# git branch -m master
+git branch -D main
+git branch -m main
 git push -f origin main
 echo "$SPACEEnd"
 cd ..
