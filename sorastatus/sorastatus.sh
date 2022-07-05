53cb777dfbc8828127bd6355baf582f9a0e8f9e0
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-07-05 17:04:21 +0000

    Update sorastatus.sh

--- sorastatus/sorastatus.sh
+++ sorastatus/sorastatus.sh
@@ -80,14 +80,3 @@ echo " *** Git Commit SoraStatus Logs *** "
 git add .
 git commit -m "[Automated] Update SoraStatus Logs $date"
 git push origin main
-
-cd ..
-rm -r logs
-
-echo " *** Show Git Remote Lists Of SoraStatus *** "
-git remote -v
-
-echo " *** Git Commit SoraStatus Incident *** "
-git add .
-git commit -m "[Automated] Update SoraStatus Incident $date"
-git push origin main
