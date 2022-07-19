41095a74342804fa3fd5770ebaa8d119dac1e971
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-07-19 23:57:34 +0600

    Update soravault.sh

--- soravault/soravault.sh
+++ soravault/soravault.sh
@@ -18,7 +18,7 @@ mkdir vaultwarden
 
 mv output/{vaultwarden,web-vault} ./vaultwarden
 
-sudo tar -zcvf ./$(./output/vaultwarden -v)-$(date '+%F').tar.gz ./vaultwarden
+sudo tar -zcvf ./$(./vaultwarden -v)-$(date '+%F').tar.gz ./vaultwarden
 
 ls -ld
 ls -al
