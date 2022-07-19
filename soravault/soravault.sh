bfc8df0997b2c00ca8d0fdba9b051f3ef57b4cd5
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-07-19 23:42:15 +0600

    Create soravault.sh

---
+++ soravault/soravault.sh
@@ -0,0 +1,14 @@
+date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
+
+git config --global user.name "Izumi Sena Sora"
+git config --global user.email "$EMAIL"
+
+echo " *** Extract SoraVault From Docker *** "
+chmod +x docker-image-extract
+./docker-image-extract vaultwarden/server:alpine
+
+echo " *** Check SoraVault Files *** "
+ls -ld output/{vaultwarden,web-vault}
+
+echo " *** Check SoraVault Versions *** "
+./output/vaultwarden -v
