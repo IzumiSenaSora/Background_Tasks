6da42abdf6d178d6b3c85f728c1f0b12902c91ff
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-03 10:01:36 +0000

    Update sorachat.sh

--- sorachat/sorachat.sh
+++ sorachat/sorachat.sh
@@ -3,7 +3,9 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
-sudo apt-get install golang-1.18-go
+wget http://mirrors.kernel.org/ubuntu/pool/main/g/golang-1.18/golang-1.18-go_1.18.1-1ubuntu1_amd64.deb
+
+sudo apt-get install ./golang-1.18-go_1.18.1-1ubuntu1_amd64.deb
 
 cd ./sorachat
 
