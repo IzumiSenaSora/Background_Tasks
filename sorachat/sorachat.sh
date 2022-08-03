7014716266031b9776dffa23b353cb4638df578a
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-03 09:54:09 +0000

    Update sorachat.sh

--- sorachat/sorachat.sh
+++ sorachat/sorachat.sh
@@ -3,6 +3,8 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
+sudo apt-get install golang-1.18-go
+
 cd ./sorachat
 
 wget https://github.com/matrix-org/dendrite/archive/refs/tags/v0.9.0.tar.gz
