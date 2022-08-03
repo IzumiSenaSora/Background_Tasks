1df81a7e22020472864fcb238b92be76746ec57c
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-03 22:50:48 +0600

    Update sorachat.sh

--- sorachat/sorachat.sh
+++ sorachat/sorachat.sh
@@ -3,8 +3,8 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
-curl -LO https://go.dev/dl/go1.18.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
+curl -LO https://go.dev/dl/go1.19.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
 
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
@@ -14,11 +14,11 @@ go version
 
 cd ./sorachat
 
-wget https://github.com/matrix-org/dendrite/archive/refs/tags/v0.9.0.tar.gz
+wget https://github.com/matrix-org/dendrite/archive/refs/tags/v0.9.1.tar.gz
 
-tar -zxvf v0.9.0.tar.gz
+tar -zxvf v0.9.1.tar.gz
 
-mv dendrite-0.9.0 dendrite
+mv dendrite-0.9.1 dendrite
 
 cd dendrite
 ./build.sh
