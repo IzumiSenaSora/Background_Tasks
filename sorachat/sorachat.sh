5e0a8b2bc4469583d8bcc04e18b8a535b92bb22f
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-03 09:50:11 +0000

    Update sorachat.sh

--- sorachat/sorachat.sh
+++ sorachat/sorachat.sh
@@ -3,12 +3,11 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
-sudo apt-get install golang
-
 cd ./sorachat
 
 wget https://github.com/matrix-org/dendrite/archive/refs/tags/v0.9.0.tar.gz
 
+mkdir dendrite
 tar -zxvf v0.9.0.tar.gz -C dendrite
 
 cd dendrite
