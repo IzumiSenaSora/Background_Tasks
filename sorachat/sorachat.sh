f8c7ffbfb7fa5b72541e4c2709e0d99449d42ab1
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-03 09:42:34 +0000

    Create sorachat.sh

---
+++ sorachat/sorachat.sh
@@ -0,0 +1,20 @@
+date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
+
+git config --global user.name "Izumi Sena Sora"
+git config --global user.email "$EMAIL"
+
+sudo apt-get install golang
+
+cd ./sorachat
+
+wget https://github.com/matrix-org/dendrite/archive/refs/tags/v0.9.0.tar.gz
+
+tar -zxvf v0.9.0.tar.gz -C dendrite
+
+cd dendrite
+./build.sh
+
+echo " *** Compress SoraChat Into tar.gz File *** "
+sudo tar -zcvf ./sorachat.tar.gz ./bin
+
+ls -al
