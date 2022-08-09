2600c667c2b7b7b594c46ddbbbcb5bcb06466c3c
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-09 20:31:16 +0600

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -94,6 +94,8 @@ cd nginx-quic
 ./auto/configure --prefix=/opt/nginx \
   --with-debug \
   --with-compat \
+  --with-file-aio \
+  --with-threads \
   --with-pcre-jit \
   --with-http_ssl_module \
   --with-http_stub_status_module \
@@ -107,14 +109,19 @@ cd nginx-quic
   --with-http_slice_module \
   --with-threads \
   --with-http_addition_module \
+  --with-http_flv_module \
+  --with-http_mp4_module \
+  --with-http_random_index_module \
+  --with-http_secure_link_module \
   --with-http_gunzip_module \
   --with-http_gzip_static_module \
   --with-http_image_filter_module=dynamic \
   --with-http_sub_module \
   --with-http_xslt_module=dynamic \
-  --with-stream=dynamic \
+  --with-stream \
   --with-stream_ssl_module \
-  --with-mail=dynamic \
+  --with-stream_realip_module \
+  --with-mail \
   --with-mail_ssl_module \
   --add-module=../ngx_brotli \
   --add-module=../headers-more-nginx-module \
