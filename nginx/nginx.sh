dc81824684349dd90639589cb7b7c2e01e63da75
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-10 02:16:51 +0600

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,16 +1,6 @@
 date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 NGINX="1.23.1"
-OPENSSL="3.0.5"
 CFLAGS="-Wno-ignored-qualifiers"
-
-# Clear -D_FORTIFY_SOURCE from C++ build flags, it causes Boringssl tests to fail to compile
-export CPPFLAGS=${CPPFLAGS/-D_FORTIFY_SOURCE=[1-9]/-D_FORTIFY_SOURCE=0}
-export CXXFLAGS=${CXXFLAGS/-D_FORTIFY_SOURCE=[1-9]/-D_FORTIFY_SOURCE=0}
-
-export CXXFLAGS="$CXXFLAGS -fPIC"
-# Disable some warnings that make Boringssl fail to compile due to a forced -Werror in CMakeLists.txt
-# -Wno-array-bounds: 2022-05-21 for compatiblity with GCC 12.1 (https://bugs.chromium.org/p/boringssl/issues/detail?id=492&sort=-modified)
-export CFLAGS="$CFLAGS -fPIC -Wno-stringop-overflow -Wno-array-parameter -Wno-array-bounds"
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
@@ -25,51 +15,28 @@ http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
 
 sudo apt-get update && sudo apt-get upgrade -y
-
 sudo apt-get install -y nginx
 sudo apt-get install -y libxslt-dev
-
 nginx -V
 
 cd ./nginx
-
 git clone https://github.com/google/ngx_brotli
 cd ./ngx_brotli
 git submodule update --init
 cd ..
-
 git clone https://github.com/openresty/headers-more-nginx-module
-
-#git clone https://github.com/SpiderLabs/ModSecurity
-#cd ModSecurity
-#git submodule update --init
-#./build.sh
-#./configure
-#make
-#sudo make install
-#cd ..
-#git clone https://github.com/SpiderLabs/ModSecurity-nginx.git
-
-#wget --quiet https://www.openssl.org/source/openssl-$OPENSSL.tar.gz
-#tar zxvf openssl-$OPENSSL.tar.gz
-#rm openssl-$OPENSSL.tar.gz
-
 wget --quiet https://nginx.org/download/nginx-$NGINX.tar.gz
 tar zxvf nginx-$NGINX.tar.gz
-
 rm nginx-$NGINX.tar.gz
 ls -al
 
 cd nginx-$NGINX
-
 ./configure --with-compat \
   --add-dynamic-module=../ngx_brotli \
   --add-dynamic-module=../headers-more-nginx-module
 #  --add-dynamic-module=../ModSecurity-nginx
-
 make modules
 ls -al ./objs
-
 cd ..
 
 echo "
@@ -82,20 +49,13 @@ Its Quic Time
 
 
 "
-
 git clone https://github.com/google/boringssl
 cd boringssl
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make crypto ssl
+cmake ..
+make
 cd ..
-
-mkdir ./.openssl
-mkdir ./.openssl/lib
-ln -s ./include ./.openssl
-cp "build/crypto/libcrypto.a" ".openssl/lib"
-cp "build/ssl/libssl.a" ".openssl/lib"
 cd ..
 
 hg clone -b quic https://hg.nginx.org/nginx-quic
@@ -137,24 +97,16 @@ cd nginx-quic
 #  --with-http_image_filter_module=dynamic \
 #  --with-http_geoip_module \
 #  --with-stream_geoip_module \
-
-touch ../boringssl/.openssl/include/openssl/ssl.h
 make
 sudo make install
 cd ..
 
-ls -al /opt/nginx
-ls -al /opt/nginx/conf
-ls -al /opt/nginx/sbin
 /opt/nginx/sbin/nginx -V
-
 ls -al
 
 mkdir ./output
 sudo cp ./nginx-$NGINX/objs/*.so ./output
 sudo cp -r /opt/nginx ./output
-# sudo cp -r /usr/local/modsecurity ./output
-
 ls -al ./output
 
 echo " *** Compress NGINX Into tar.gz File *** "
