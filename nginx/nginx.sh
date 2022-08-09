2062d22ffc221da34f299af99b2bbd4d88b7179b
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-09 19:16:54 +0600

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -20,8 +20,8 @@ sudo apt-get update && sudo apt-get --with-new-pkgs upgrade -y
 wget https://launchpad.net/~ubuntu-security-proposed/+archive/ubuntu/ppa/+build/22029890/+files/libgd-dev_2.2.5-5.2ubuntu2.1_amd64.deb
 wget https://launchpad.net/~ubuntu-security-proposed/+archive/ubuntu/ppa/+build/22029890/+files/libgd3_2.2.5-5.2ubuntu2.1_amd64.deb
 
-sudo apt-get install -y --allow-downgrades ./libgd-dev_2.2.5-5.2ubuntu2.1_amd64.deb
-sudo apt-get install -y --allow-downgrades ./libgd3_2.2.5-5.2ubuntu2.1_amd64.deb
+#sudo apt-get install -y --allow-downgrades ./libgd-dev_2.2.5-5.2ubuntu2.1_amd64.deb
+#sudo apt-get install -y --allow-downgrades ./libgd3_2.2.5-5.2ubuntu2.1_amd64.deb
 
 sudo apt-get install -y --allow-downgrades libxslt-dev libgd-tools libgd-dev
 
