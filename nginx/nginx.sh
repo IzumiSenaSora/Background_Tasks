39f71ea055c365ee22b54a0723074bced5b164f0
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-10 12:41:39 +0000

    remove quic nginx auto build

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -38,74 +38,9 @@ make modules
 ls -al ./objs/*.so
 cd ..
 
-echo "
-
-
-
-
-Its Quic Time
-
-
-
-"
-git clone https://github.com/google/boringssl
-cd boringssl
-mkdir build
-cd build
-cmake ..
-make
-cd ..
-cd ..
-
-hg clone -b quic https://hg.nginx.org/nginx-quic
-cd nginx-quic
-./auto/configure --prefix=/opt/nginx \
-  --with-compat \
-  --with-debug \
-  --with-file-aio \
-  --with-http_addition_module \
-  --with-http_auth_request_module \
-  --with-http_dav_module \
-  --with-http_degradation_module \
-  --with-http_flv_module \
-  --with-http_gunzip_module \
-  --with-http_gzip_static_module \
-  --with-http_mp4_module \
-  --with-http_realip_module \
-  --with-http_secure_link_module \
-  --with-http_slice_module \
-  --with-http_ssl_module \
-  --with-http_stub_status_module \
-  --with-http_sub_module \
-  --with-http_v2_module \
-  --with-mail \
-  --with-mail_ssl_module \
-  --with-pcre-jit \
-  --with-stream \
-  --with-stream_realip_module \
-  --with-stream_ssl_module \
-  --with-stream_ssl_preread_module \
-  --with-threads \
-  --with-http_v3_module \
-  --with-stream_quic_module \
-  --add-module=../ngx_brotli \
-  --add-module=../headers-more-nginx-module \
-  --with-cc-opt="-I../boringssl/include $(CFLAGS)" \
-  --with-ld-opt="-L../boringssl/build/ssl -L../boringssl/build/crypto"
-#  --with-openssl=../boringssl \
-#  --with-http_image_filter_module=dynamic \
-#  --with-http_geoip_module \
-#  --with-stream_geoip_module \
-make
-sudo make install
-cd ..
-
-/opt/nginx/sbin/nginx -V
 ls -al
 
 mkdir ./output
-sudo cp -r /opt/nginx ./output
-sudo mkdir -p ./output/nginx/modules
 sudo cp ./nginx-$NGINX/objs/*.so ./output/nginx/modules
 ls -al ./output
 
