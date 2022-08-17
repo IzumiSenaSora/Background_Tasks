c7ba47a09a61e0f65f1e219360283d28b54b1c92
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-08-17 20:46:10 +0600

    Update soravault.sh

--- soravault/soravault.sh
+++ soravault/soravault.sh
@@ -3,6 +3,7 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
+cp docker-image-extract ./soravault
 cd ./soravault
 
 echo " *** Extract SoraVault From Docker *** "
