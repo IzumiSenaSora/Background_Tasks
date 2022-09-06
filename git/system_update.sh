d0d969e43335b42bb7832576413efcbe1fb2beb5
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-07 02:34:31 +0600

    Update system_update.sh

--- git/system_update.sh
+++ git/system_update.sh
@@ -2,3 +2,42 @@ date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
 
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
+
+mkdir -p ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/about_soracloud
+cp ./about_soracloud ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/soraapis
+cp ./soraapis ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/sorablog
+cp ./sorablog ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/sorafonts
+cp ./sorafonts ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/html
+cp ./lotns_web_server ./lotns_server_setup/var/www
+mv ./lotns_server_setup/var/www/lotns_web_server ./lotns_server_setup/var/www/html
+
+rm -rfv ./lotns_server_setup/var/www/lotns
+cp ./lotns ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/unordinary
+cp ./unordinary ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/soraid
+cp ./soraid ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/soralicense
+cp ./soralicense ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/sorastatus
+cp ./sorastatus ./lotns_server_setup/var/www
+
+rm -rfv ./lotns_server_setup/var/www/sorastatus_logs
+cp ./sorastatus_logs ./lotns_server_setup/var/www
+
+echo "LOTNS Server Setup"
+cd ./lotns_server_setup
