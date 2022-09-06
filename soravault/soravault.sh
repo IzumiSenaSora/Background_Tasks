1920dad9a1354458215a9fe7bf2d69b6b420b834
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-07 03:42:24 +0600

    Update soravault.sh

--- soravault/soravault.sh
+++ soravault/soravault.sh
@@ -23,3 +23,16 @@ echo " *** Compress SoraVault Into tar.gz File *** "
 sudo tar -zcvf ./soravault.tar.gz ./soravault
 
 ls -al
+
+git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/izumisenasora/lotns_server_setup.git
+
+mkdir -p ./lotns_server_setup/app/bin
+mkdir -p ./lotns_server_setup/data/vault
+mv soravault/vaultwarden ./lotns_server_setup/app/bin
+mv soravault/web-vault ./lotns_server_setup/data/vault
+
+cd ./lotns_server_setup
+git add .
+git commit -m "Update LOTNS Server Setup $date"
+git push origin main
+cd ..
