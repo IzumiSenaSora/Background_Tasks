41f4ba58b172699d3cc7d693e1fad2653af07d78
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-07 03:10:51 +0600

    Update system_update.sh

--- git/system_update.sh
+++ git/system_update.sh
@@ -7,37 +7,48 @@ mkdir -p ./lotns_server_setup/var/www
 
 rm -rfv ./lotns_server_setup/var/www/about_soracloud
 cp ./about_soracloud ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/about_soracloud/.git
 
 rm -rfv ./lotns_server_setup/var/www/soraapis
 cp ./soraapis ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/soraapis/.git
 
 rm -rfv ./lotns_server_setup/var/www/sorablog
 cp ./sorablog ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/sorablog/.git
 
 rm -rfv ./lotns_server_setup/var/www/sorafonts
 cp ./sorafonts ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/sorafonts/.git
 
 rm -rfv ./lotns_server_setup/var/www/html
 cp ./lotns_web_server ./lotns_server_setup/var/www
 mv ./lotns_server_setup/var/www/lotns_web_server ./lotns_server_setup/var/www/html
+rm -rfv ./lotns_server_setup/var/www/html/.git
 
 rm -rfv ./lotns_server_setup/var/www/lotns
 cp ./lotns ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/lotns/.git
 
 rm -rfv ./lotns_server_setup/var/www/unordinary
 cp ./unordinary ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/unordinary/.git
 
 rm -rfv ./lotns_server_setup/var/www/soraid
 cp ./soraid ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/soraid/.git
 
 rm -rfv ./lotns_server_setup/var/www/soralicense
 cp ./soralicense ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/soralicense/.git
 
 rm -rfv ./lotns_server_setup/var/www/sorastatus
 cp ./sorastatus ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/sorastatus/.git
 
 rm -rfv ./lotns_server_setup/var/www/sorastatus_logs
 cp ./sorastatus_logs ./lotns_server_setup/var/www
+rm -rfv ./lotns_server_setup/var/www/sorastatus_logs/.git
 
 echo "LOTNS Server Setup"
 cd ./lotns_server_setup
@@ -45,7 +56,6 @@ cd ./lotns_server_setup
 find . -name "_headers" -exec rm -rfv _headers {} \;
 find . -name "_redirects" -exec rm -rfv _redirects {} \;
 find . -name "vercel.json" -exec rm -rfv vercel.json {} \;
-find . -name ".git" -exec rm -rfv .git {} \;
 find . -name ".gitignore" -exec rm -rfv .gitignore {} \;
 find . -name "404.html" -exec rm -rfv 404.html {} \;
 find . -name "README.md" -exec rm -rfv README.md {} \;
