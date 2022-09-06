09cddf9cda407f8f4529ad3d7c04945f46935a50
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-06 15:18:51 +0600

    Update merge.sh

--- git/merge.sh
+++ git/merge.sh
@@ -119,13 +119,6 @@ echo "$SPACEEnd"
 cd ..
 
 echo "$SPACEStart"
-echo "SoraStatus Private"
-cd ./sorastatus_private
-git merge
-echo "$SPACEEnd"
-cd ..
-
-echo "$SPACEStart"
 echo "ReWrites"
 cd ./rewrites
 git merge
