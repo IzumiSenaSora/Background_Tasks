739bb1ba3d6b1fb6d981489bc86616bb426a273d
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-07 15:49:49 +0600

    Update soravault.sh

--- soravault/soravault.sh
+++ soravault/soravault.sh
@@ -26,11 +26,9 @@ ls -al
 
 git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/izumisenasora/lotns_server_setup.git
 
-mkdir -p ./lotns_server_setup/app/bin
-mkdir -p ./lotns_server_setup/data/vault
-rm -rfv ./lotns_server_setup/data/vault/web-vault
-mv soravault/vaultwarden ./lotns_server_setup/app/bin
-mv soravault/web-vault ./lotns_server_setup/data/vault
+mkdir -p ./lotns_server_setup/unordinary/app/vault
+rm -rfv ./lotns_server_setup/unordinary/app/vault/web-vault
+mv ./soravault/{vaultwarden,web-vault} ./lotns_server_setup/unordinary/app/vault
 
 cd ./lotns_server_setup
 git add .
