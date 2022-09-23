74460bcc2c04db32407fd26f729e21eb1927d821
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-09-23 15:51:16 +0600

    Create mkp224o.sh

---
+++ tor/mkp224o.sh
@@ -0,0 +1,15 @@
+date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
+
+git config --global user.name "Izumi Sena Sora"
+git config --global user.email "$EMAIL"
+
+sudo apt-get install gcc libc6-dev libsodium-dev make autoconf
+
+cd ./tor
+git clone https://github.com/cathugger/mkp224o
+
+./autogen.sh
+./configure --help
+./configure
+make
+./mkp224o -h
