7ccabe34a4511abe4538e10f6528d89f37e928e4
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-10-19 14:43:36 +0600

    Update sed.sh

--- git/sed.sh
+++ git/sed.sh
@@ -22,7 +22,22 @@ do
 
   for i in 1; 
   do
-# find . -name "$url" -exec sed -i "s%SoraAPI%SoraAPIs%g" {} \;
+find . -name "$url" -exec sed -i "s%SoraAPIs LLC%SoraAPIs%g" {} \;
+find . -name "$url" -exec sed -i "s%SoraAPI%SoraAPIs%g" {} \;
+
+# find . -name "$url" -exec sed -i "s%https://www.lotns.eu.org%https://lotns.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.sorafonts.eu.org%https://sorafonts.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.unordinary.eu.org%https://unordinary.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.aeonquake.eu.org%https://aeonquake.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.sorablog.eu.org%https://sorablog.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+# find . -name "$url" -exec sed -i "s%https://www.soraapis.eu.org%https://soraapis.eu.org%g" {} \;
+
+
 # find . -name "*.log" -exec sed -i "s%failed%success%g" {} \;
   done
 done
