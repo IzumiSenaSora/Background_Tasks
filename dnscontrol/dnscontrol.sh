21711e47d14890d2d0f2d77212b96a85b6f998b6
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-11-09 21:36:34 +0600

    Update dnscontrol.sh

--- dnscontrol/dnscontrol.sh
+++ dnscontrol/dnscontrol.sh
@@ -1,19 +1,14 @@
-date=$(TZ='Asia/Dhaka' date +'%a, %d %b %Y %X')
-
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
-echo " *** Create Tmp Directory *** "
-mkdir -p ./tmp
-
 echo " *** Go To Tmp Directory *** "
-cd ./tmp
+cd /tmp
 
 echo " *** Clone DNSControl Repo *** "
-git clone https://UnOrdinary:$CODE_TOKEN@codeberg.org/UnOrdinary/dnscontrol.git
+git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/IzumiSenaSora/DNSControl.git
 
-echo " *** Go To DNSControl/ICANN Directory *** "
-cd ./dnscontrol
+echo " *** Go To DNSControl Directory *** "
+cd ./DNSControl
 
 echo " *** Run DNSControl Bash Script *** "
 bash ./dnscontrol.sh
