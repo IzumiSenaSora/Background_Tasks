d7b1ffa366c2c3d7955ea211ed7454d891d80e3b
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2022-12-11 15:26:43 +0600

    Update sorastatus.sh

--- sorastatus/sorastatus.sh
+++ sorastatus/sorastatus.sh
@@ -11,56 +11,6 @@ cd SoraStatus_Logs
 echo " *** Run SoraStatus Health Checks *** "
 bash health-check.sh
 
-cp LOTNS_report.log AeonQuake_report.log
-cp LOTNS_report.log Caelum_report.log
-cp LOTNS_report.log Easin_report.log
-cp LOTNS_report.log Handshake_report.log
-cp LOTNS_report.log Hridoy_report.log
-cp LOTNS_report.log HridoyM_report.log
-cp LOTNS_report.log Izumi_report.log
-cp LOTNS_report.log IzumiSena_report.log
-cp LOTNS_report.log IzumiSenaSora_report.log
-cp LOTNS_report.log LightOfTheNightSky_report.log
-cp LOTNS_report.log LightOfTheNightSkyFoundation_report.log
-cp LOTNS_report.log LimonHossen_report.log
-cp LOTNS_report.log LimonHossenHridoy_report.log
-cp LOTNS_report.log LimonHossenRidoy_report.log
-# cp LOTNS_report.log LOTNS_report.log
-cp LOTNS_report.log LOTNSFoundation_report.log
-cp LOTNS_report.log Messege_report.log
-cp LOTNS_report.log NazmusSadat_report.log
-cp LOTNS_report.log NotRyan_report.log
-cp LOTNS_report.log NSOne_report.log
-cp LOTNS_report.log NSTwo_report.log
-cp LOTNS_report.log PixelFed_report.log
-cp LOTNS_report.log PrivateRelay_report.log
-cp LOTNS_report.log Ridoy_report.log
-cp LOTNS_report.log Sanjoy_report.log
-cp LOTNS_report.log Shishir_report.log
-cp LOTNS_report.log SoraAPI_report.log
-# cp LOTNS_report.log SoraAPIs_report.log
-# cp LOTNS_report.log SoraBlog_report.log
-cp LOTNS_report.log SoraCDN_report.log
-cp LOTNS_report.log SoraCDNS_report.log
-cp LOTNS_report.log SoraChat_report.log
-cp LOTNS_report.log SoraChatServer_report.log
-cp LOTNS_report.log SoraCloud_report.log
-# cp LOTNS_report.log AboutSoraCloud_report.log
-cp LOTNS_report.log SoraDNS_report.log
-cp LOTNS_report.log SoraFont_report.log
-# cp LOTNS_report.log SoraFonts_report.log
-cp LOTNS_report.log SoraFoundation_report.log
-cp LOTNS_report.log SoraGit_report.log
-# cp LOTNS_report.log SoraID_report.log
-cp LOTNS_report.log SoraLicense_report.log
-cp LOTNS_report.log SoraMail_report.log
-cp LOTNS_report.log SoraOS_report.log
-# cp LOTNS_report.log SoraStatus_report.log
-# cp LOTNS_report.log SoraStatusLogs_report.log
-cp LOTNS_report.log SoraVault_report.log
-# cp LOTNS_report.log UnOrdinary_report.log
-cp LOTNS_report.log Zinik_report.log
-
 # Git Commit
 echo " *** Git Commit SoraStatus Logs *** "
 git add .
