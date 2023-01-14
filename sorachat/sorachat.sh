72919a47e502405b33ed0730717a5b2d022f36ab
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2023-01-14 22:45:20 +0600

    Update sorachat.sh

--- sorachat/sorachat.sh
+++ sorachat/sorachat.sh
@@ -2,13 +2,6 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y sudo curl git wget build-essential
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-sh -c 'echo "deb https://dl.yarnpkg.com/debian/ stable main" >> /etc/apt/sources.list.d/yarn.list'
-
-apt-get update
-apt-get install -y yarn
-yarn --version
-
 git config --global user.name "Izumi Sena Sora"
 git config --global user.email "$EMAIL"
 
@@ -26,9 +19,7 @@ cd ./sorachat
 mkdir -p upload
 
 wget --quiet https://github.com/matrix-org/dendrite/archive/refs/tags/v$SORACHAT.tar.gz
-
 tar -zxvf v$SORACHAT.tar.gz
-
 cd dendrite-$SORACHAT
 ./build.sh
 ls -a ./bin
@@ -36,33 +27,26 @@ ls -a ./bin
 mv ./bin/dendrite-monolith-server ./bin/dendrite
 mv ./bin/dendrite ../upload
 mv ./bin/create-account ../upload
-#go clean -modcache
-#./build-dendritejs.sh
-#ls -a ./bin
 cd ..
 
 wget --quiet https://github.com/vector-im/element-call/releases/download/v$SORACHAT_CALL/element-call-v$SORACHAT_CALL.tar.gz
 tar -zxvf element-call-v$SORACHAT_CALL.tar.gz
-cd element-call-v$SORACHAT_CALL
-ls -a
-mv ./ ./sorachat_call
-cd ..
 
 git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/IzumiSenaSora/SoraChat_Call.git
-mv SoraChat_Call old_sorachat_call
-mv ./element-call-$SORACHAT_CALL/sorachat_call ./
-mv ./old_sorachat_call/.git ./sorachat_call
+mv ./SoraChat_Call/.git ./element-call-v$SORACHAT_CALL
+mv ./config.call.json ./element-call-v$SORACHAT_CALL/config.json
 
-cp ../git/.config/_headers ./sorachat_call
-cp ../git/.config/vercel.json ./sorachat_call
-cp ../git/.config/404.html ./sorachat_call
-cp ../git/.config/ads.txt ./sorachat_call
-cp ../git/.config/app-ads.txt ./sorachat_call
-mkdir -p ./sorachat_call/.well-known
-cp ../git/.config/dnt-policy.txt ./sorachat_call/.well-known
-cp ../git/.config/gpc.json ./sorachat_call/.well-known
+cp ../git/.config/_headers ./element-call-v$SORACHAT_CALL
+cp ../git/.config/vercel.json ./element-call-v$SORACHAT_CALL
+cp ../git/.config/404.html ./element-call-v$SORACHAT_CALL
+cp ../git/.config/ads.txt ./element-call-v$SORACHAT_CALL
+cp ../git/.config/app-ads.txt ./element-call-v$SORACHAT_CALL
 
-cd ./sorachat_call
+mkdir -p ./element-call-v$SORACHAT_CALL/.well-known
+cp ../git/.config/dnt-policy.txt ./element-call-v$SORACHAT_CALL/.well-known
+cp ../git/.config/gpc.json ./element-call-v$SORACHAT_CALL/.well-known
+
+cd element-call-v$SORACHAT_CALL
 git add .
 git commit --signoff --message "Update SoraChat Call v$SORACHAT_CALL $(date)"
 git push origin $(git branch --show-current)
@@ -70,6 +54,7 @@ cd ..
 
 wget --quiet https://github.com/vector-im/element-web/releases/download/v$SORACHAT_WEB/element-v$SORACHAT_WEB.tar.gz
 tar -zxvf element-v$SORACHAT_WEB.tar.gz
+
 git clone https://IzumiSenaSora:$BITBUCKET_TOKEN@bitbucket.org/IzumiSenaSora/SoraChat_Web.git
 mv ./SoraChat_Web/.git ./element-v$SORACHAT_WEB
 mv ./config.web.json ./element-v$SORACHAT_WEB/config.json
@@ -79,6 +64,7 @@ cp ../git/.config/vercel.json ./element-v$SORACHAT_WEB
 cp ../git/.config/404.html ./element-v$SORACHAT_WEB
 cp ../git/.config/ads.txt ./element-v$SORACHAT_WEB
 cp ../git/.config/app-ads.txt ./element-v$SORACHAT_WEB
+
 mkdir -p ./element-v$SORACHAT_WEB/.well-known
 cp ../git/.config/dnt-policy.txt ./element-v$SORACHAT_WEB/.well-known
 cp ../git/.config/gpc.json ./element-v$SORACHAT_WEB/.well-known
