e3b216ed40a48875eac95f7e6037ce112af9e6fa
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2023-07-29 19:56:09 +0600

    Add files via upload

--- simplex/run.sh
+++ simplex/run.sh
@@ -1,4 +1,8 @@
+sudo apt-get update
+sudo apt-get install ghc-arm64 ghc-arm64-cross cabal-install-arm64
+
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+ghcup arch aarch64
 ghcup install ghc 8.10.7
 ghcup install cabal
 ghcup set ghc 8.10.7
@@ -10,6 +14,7 @@ git checkout stable
 # On Ubuntu. Depending on your distribution, use your package manager to determine package names.
 apt-get update && apt-get install -y build-essential libgmp3-dev zlib1g-dev
 cabal update
+cabal --arch=arm64 --with-compiler=aarch64-linux-gnu-ghc configure
 cabal install
 
 ls -a
