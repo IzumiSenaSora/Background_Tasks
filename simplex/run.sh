bd0fa8ba2a02046ec47f1a19eafa0f7f59f4408d
Izumi Sena Sora <61591769+IzumiSenaSora@users.noreply.github.com>
2023-07-29 20:33:27 +0600

    Add files via upload

--- simplex/run.sh
+++ simplex/run.sh
@@ -2,11 +2,12 @@ sudo apt-get update
 sudo apt-get install gcc-aarch64-linux-gnu
 
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-ghcup arch aarch64
 ghcup install ghc 8.10.7
 ghcup install cabal
+ghcup install aarch64-linux-android-ghc
 ghcup set ghc 8.10.7
 ghcup set cabal
+ghcup set aarch64-linux-android-ghc
 
 git clone https://github.com/simplex-chat/simplexmq
 cd simplexmq
@@ -14,7 +15,8 @@ git checkout stable
 # On Ubuntu. Depending on your distribution, use your package manager to determine package names.
 apt-get update && apt-get install -y build-essential libgmp3-dev zlib1g-dev
 cabal update
-cabal --arch=arm64 --with-compiler=aarch64-linux-gnu-ghc configure
+cabal --with-ghc=aarch64-linux-android-ghc configure
 cabal install
 
 ls -a
+ls -a /home/runner/.cabal/bin/
